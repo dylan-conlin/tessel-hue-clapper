c58029e0be2b0bf2fd478d4dd2fa850307907cec
Dylan Conlin <dylan.conlin@gmail.com>
2015-12-30 15:52:33 -0800

    working except for double reqeusts

--- index.js
+++ index.js
@@ -8,72 +8,23 @@ var tessel = require('tessel');
 var ambientlib = require('ambient-attx4');
 var ambient = ambientlib.use(tessel.port['B']);
 var http = require('http');
+var hueApiKey = '1d456a84386935a222fc058f36b3f57f';
+var togglingLights = false;
 
-
-ambient.on('ready', function () {
-  // Get points of light and sound data.
-  setInterval( function () {
-    ambient.getLightLevel( function(err, ldata) {
-      ambient.getSoundLevel( function(err, sdata) {
-        console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
-      });
-    })}, 500); // The readings will happen every .5 seconds unless the trigger is hit
-
-  // Set a sound level trigger
-  // The trigger is a float between 0 and 1
-  ambient.setSoundTrigger(0.1);
-
-  ambient.on('sound-trigger', function(data) {
-    console.log("Something happened with sound: ", data);
-    
-
-    turn_lights(false);
-    
-    // Clear it
-    ambient.clearSoundTrigger();
-
-    //After 1.5 seconds reset sound trigger
-    setTimeout(function () { 
-
-      ambient.setSoundTrigger(0.1);
-
-    },1500);
-
-  });
-});
-
-ambient.on('error', function (err) {
-  console.log(err)
-});
-
-
-var toggle_lights = function(lights, state){
-  for(var i = 0; i < lights.length; i++) {
-    console.log("lights: ", lights)
-    console.log("state: ", state)
-    toggle_light(i, state)
-  }
-}
-
-var toggle_light = function(light, state) {
-  var lightPath = "/api/1d456a84386935a222fc058f36b3f57f/lights/" + light + "/state/";
-  console.log(lightPath);
+function toggleLights(state, cb) {
   var put_options = {
     host: '192.168.1.3',
     port: '80',
-    path: lightPath,
+    path: "/api/1d456a84386935a222fc058f36b3f57f/groups/0/action",
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     }
   };
-
   var req = http.request(put_options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
     res.setEncoding('utf8');
     res.on('data', function (body) {
-      console.log('BODY: ' + body);
+      cb(body)
     });
   });
 
@@ -81,17 +32,20 @@ var toggle_light = function(light, state) {
     console.log('problem with request: ' + e.message);
   });
 
-  // write data to request body
-  req.write('{"on": ' + state + '}');
-  req.end();
-
+  req.write('{"on":' + state + '}');
+  req.end(function(){ambient.setSoundTrigger(0.1)});
 }
 
-var turn_lights = function(state){
+function handleClap(data){
+  console.log("Something happened with sound: ", data);
+
+  // Clear soundTrigger
+  ambient.clearSoundTrigger();
+
   var get_options = {
     host: '192.168.1.3',
     port: '80',
-    path: '/api/1d456a84386935a222fc058f36b3f57f/lights',
+    path: '/api/' + hueApiKey + '/lights',
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -99,24 +53,41 @@ var turn_lights = function(state){
   };
 
   var req = http.request(get_options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
     res.setEncoding('utf8');
     res.on('data', function (body) {
-      console.log(typeof body)
-      var lights = Object.keys(JSON.parse(body))
-      console.log("lights: ", lights)
-      // console.log("state: ", state)
-      toggle_lights(lights, state);
+      // JSON.parse doesnt work on tessel for some reason.
+      var lightsAreOn = body.indexOf('{"on":true,') > -1 ? false : true
+      console.log(lightsAreOn)
+      toggleLights(lightsAreOn, function(body){
+        console.log(body)
+      })
     });
   });
-
+  
   req.on('error', function(e) {
     console.log('problem with request: ' + e.message);
   });
 
-  // write data to request body
-  // req.write('{"on": true}');
   req.end();
 
-}
+};
+
+
+ambient.on('ready', function () {
+  
+  console.log('Listening...')
+  
+  // Set a sound level trigger
+  // The trigger is a float between 0 and 1
+  
+  ambient.setSoundTrigger(0.1);
+
+  ambient.on('sound-trigger', handleClap)
+});
+
+ambient.on('error', function (err) {
+  console.log(err);
+});
+
+
+
